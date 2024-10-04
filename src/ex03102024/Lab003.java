ff74444d737e326b27db052814c23d99873be1cf
Hemanth <hemanthemman70@gmail.com>
2024-10-04 07:33:48 +0530

    My files

--- src/ex03102024/Lab003.java
+++ src/ex03102024/Lab003.java
@@ -1,4 +1,10 @@
 package ex03102024;
 
 public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("10+20");
+        System.out.println("40-10");
+        System.out.println("50*3");
+        System.out.println("50/3");
+    }
 }
