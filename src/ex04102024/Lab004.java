733eea4af0271b7be9d72962584bc08d55708577
Hemanth <hemanthemman70@gmail.com>
2024-10-09 10:51:31 +0530

    My files

---
+++ src/ex04102024/Lab004.java
@@ -0,0 +1,8 @@
+package ex04102024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println(10+16);
+        System.out.println(10-12);
+    }
+}
---
+++ src/ex08102024/Lab005.java
@@ -0,0 +1,4 @@
+package ex08102024;
+
+public class Lab005 {
+}
