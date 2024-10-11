808606ec5e8f7cf1c0cd1677b144848c75fc2287
Hemanth <hemanthemman70@gmail.com>
2024-10-11 20:43:22 +0530

    My files

---
+++ src/ex10102024/Lab008.java
@@ -0,0 +1,17 @@
+package ex10102024;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        int a=10, b=5;
+
+        System.out.println(a>b); // true
+        System.out.println(a<b); // false
+        System.out.println(a>=b);  // true
+        System.out.println(a<=b);  // false
+        System.out.println(a!=b);  // ture
+        System.out.println(a==b);  // false
+        b=10;
+        System.out.println(a==b);  // true
+        System.out.println(a!=b);  //  false
+    }
+}
