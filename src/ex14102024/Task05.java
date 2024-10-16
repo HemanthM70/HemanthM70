43b12a0f6eef6d79d054d1afa9172eca11fa9c77
Hemanth <hemanthemman70@gmail.com>
2024-10-16 11:31:09 +0530

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Hemanth <hemanthemman70@gmail.com>

--- src/ex14102024/Task05.java
+++ src/ex14102024/Task05.java
@@ -2,9 +2,9 @@ package ex14102024;
 
 public class Task05 {
     public static void main(String[] args) {
-        int a = -5;
+        int a = 20;
         int b = 20;
-        int c = -10;
+        int c = 30;
 
         int result = (a >= b && a >= c) ? a : (b >= c ? b : c);
         System.out.println("Max Number is -> " +  result);
